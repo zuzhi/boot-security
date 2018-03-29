511eae00d76faf170c9a351558638a675f6ad1eb
zuzhi <476513451@qq.com>
2018-03-30 00:15:08 +0800

    Add HelloWorldController

--- src/main/java/com/example/bootsecurity/DemoApplication.java
+++ src/main/java/com/example/bootsecurity/DemoApplication.java
@@ -2,11 +2,28 @@ package com.example.bootsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * DemoApplication
+ *
+ * @author zuzhi
+ * @date 29/03/2018
+ */
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @RestController
+    class HelloController {
+        @RequestMapping(value = "/", method = RequestMethod.GET)
+        public String hello() {
+            return "Hello, World!";
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
