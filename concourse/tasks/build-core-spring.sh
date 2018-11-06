0dd0db3d29dddbee7f0606ba49c5d1bf449b494b
zuzhi <476513451@qq.com>
2018-11-06 18:13:07 +0800

    Set up CI with Concourse

---
+++ concourse/tasks/build-core-spring.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e -u -x
+
+chown gradle:gradle -R ./core-spring-repo
+cd core-spring-repo
+su gradle -c "./gradlew --console plain build"
+cd ../
+cp -R core-spring-repo/build/* build
